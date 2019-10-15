dff055836b1ca6622401b5679c7e9a939e3bbe53
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-10-15 23:06:24 +0000

    print

--- app.js
+++ app.js
@@ -7,20 +7,26 @@ var path = require('path');
 
 // constants in js never change
 const express = require("express"); // I need this in the library
+
+var bodyParser = require("body-parser");
+
 const app = express(); // calling express
 const port = process.env.PORT || 3000; // port established as constant for maintability
 
+
 app.set("views", path.join(__dirname, 'views'));
-
 app.set("view engine", 'ejs');
-
+app.use(bodyParser.urlencoded({ encoded: true}));
+var task = ["yeet", "yote"];
 
 app.get('/', function(req, res){
     res.render("index");
 });
 
-app.get('/about', function(req, res){
-    res.send('<h1>sup beech this about me</h1>');
+app.post('/addTask', function(req, res){
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect('/');
 
 });
 http.createServer(app).listen(port, function(){ //will not run in heroku, porte numberh
