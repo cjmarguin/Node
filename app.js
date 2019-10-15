1388aa67bc8a19b44e221a51f2f0c63fa87e3d58
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-10-15 23:15:24 +0000

    constants to varibles

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var http = require('http');
 var path = require('path');
 
 
-// constants in js never change
-const express = require("express"); // I need this in the library
+// constants (const) in js never change
+var express = require("express"); // I need this in the library
 
 var bodyParser = require("body-parser");
 
-const app = express(); // calling express
-const port = process.env.PORT || 3000; // port established as constant for maintability
+var app = express(); // calling express
+var port = process.env.PORT || 3000; // port established as constant for maintability
 
 
 app.set("views", path.join(__dirname, 'views'));
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ encoded: true}));
 var task = ["yeet", "yote"];
 
 app.get('/', function(req, res){
-    res.render("index");
+    res.render("index", {task:task});
 });
 
 app.post('/addTask', function(req, res){
