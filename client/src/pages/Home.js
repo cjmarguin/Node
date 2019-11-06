3ae742509b5d02b5293d0a5c10c1b931ec94b67c
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-11-06 00:08:45 +0000

    yionk react

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+
+    // write the function to do the fetch
+
+    render(){
+        return(
+            <h1> BEECH CANOES</h1>
+        );
+    }
+}
+
+export default Home;
